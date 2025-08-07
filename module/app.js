8a644470735f9cc77880ea0fcf3d63ee28c33493
RAFAELLA MARIA TOSCANO REIS <24122319@dsn127m1249386.cfp127aluno.local>
2025-08-07 12:23:08 -0300

    render ok

--- module/app.js
+++ module/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 export const getPhotos = async () => {
-  const url = 'https://projeto-fotos-1.onrender.com'
+  const url = 'https://projeto-fotos-1.onrender.com/fotos'
   const response = await fetch(url)
   const data = await response.json()
   return data
